d04d3532372ad2551f8f4c4db29ef62bd88f76c3
alex sanchez <alexsanchez.az2005@gmail.com>
2025-05-26 23:55:19 -0500

    actualizacion inicial

--- resources/views/Gestion_usuarios/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{{ asset('assets/js/jquery-3.7.0.min.js') }}"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
